3f18163521ba73b1da9d491e82fe95a4a1a3cfb3
nathan-alfatah <125951254+nathan-alfatah@users.noreply.github.com>
2023-05-19 10:34:10 +0700

    Add files via upload

---
+++ JavaScript/HelloWorld.js
@@ -0,0 +1,8 @@
+document.writeln("Hello World");
+//perkenalan
+document.writeln(<textarea cols="100" rows="10"></textarea>);
+document.writeln("</br> Nama: Nathan Al Fattah Zen");
+document.writeln("</br> NIM: 20105244035");
+document.writeln("</br> Kelas: Teknologi Pendidikan A");
+
+document.writeln(0b10101);
